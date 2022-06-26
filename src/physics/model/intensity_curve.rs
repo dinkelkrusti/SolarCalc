cb942e15ded6189845d30f0166130d170682ec97
ERIK_WITTEMEIER <erik.wittemeier@gmail.com>
2022-06-27 00:46:45 +0200

    feat: add basics for power gen model

---
+++ src/physics/model/intensity_curve.rs
@@ -0,0 +1,60 @@
+//
+// Calculates the intensity of sunlight in [J/(m^2*second)] at:
+//
+// - given standard time,
+// - given day -> 1...365
+// - given location on the globe -> location
+// - cloudiness factor in interval -> [0,1],
+// - angle of the receiver wrt. ground (equatorial bound and pi/2 means horizontal)
+
+// TODO: not yet implemented
+pub fn intensity(
+    time: u32,
+    day: u16,
+    cloudyness: f64,
+    angle_receiver: f64,
+    loc: Location
+) -> f64 {
+
+    let mut intensity = 0f64;
+
+    return intensity;
+}
+
+// Calculates the power output of a panel configuration in [J/s] with
+// - specified area in square meters
+// - specified efficiency
+// - at a given sun intensity in [J/(m^2*second)]
+pub fn panel_output(
+    intensity: f64,
+    area: f64,
+    efficiency: f64
+) -> f64 {
+    return intensity*area*intensity;
+}
+
+// TODO: not yet implemented
+pub fn angle_sun(
+    time: u32,
+    day: u16,
+    loc: Location
+)-> f64 {
+    return 0f64;
+}
+
+// TODO: not yet implemented
+pub fn angle_sun_receiver(
+    time: u32,
+    day: u16,
+    loc: Location,
+    angle_receiver: f64
+) -> f64 {
+    return 0f64;
+}
+
+
+// Position on the surface of the earth
+pub struct Location {
+    latitude: f64,
+    longitude: f64,
+}
