6092e8929567b93f285621bfca88c49458fc857e
ERIK_WITTEMEIER <erik.wittemeier@gmail.com>
2022-06-26 23:50:40 +0200

    feat: add std deviation calculation

--- src/math/moments.rs
+++ src/math/moments.rs
@@ -2,10 +2,21 @@ pub fn mean(input: Vec<f64>) -> f64 {
     let len = input.len();
 
     let mut result = 0f64;
-    for element in input {
-        result += element;
+    for e in input {
+        result += e;
     }
 
     let result = result/len;
     return(result);
+}
+
+pub fn standard_deviation(input: Vec<f64>) -> f64 {
+    let mean = self::mean(input);
+    let mut variance: f64 = 0f64;
+
+    for e in input {
+        variance += (e-mean).pow(2);
+    }
+
+    return variance.sqrt();
 }
